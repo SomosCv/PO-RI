2d816212d4f4b66eeaa57dc48072fb5501da2053
Somos.Cv <somoscvapp@gmail.com>
2026-05-26 21:32:50 -0400

    Add files via upload

--- data/lightshow-images.js
+++ data/lightshow-images.js
@@ -1,19 +1,25 @@
+const isMobile = window.innerWidth <= 1024;
+
+const folder = isMobile
+    ? 'images/9-16-vertical'
+    : 'images/16-9-horizontal';
+
 window.LIGHTSHOW_IMAGES = {
 
     recovery: [
-        'images/cover.png',
-        'images/overdose2.png',
-        'images/prevention-centers.png',
+        `${folder}/1.png`,
+        `${folder}/2.png`,
+        `${folder}/3.png`,
     ],
 
     overdose: [
-        'images/addiction.png',
-        'images/resources.png',
+        `${folder}/4.png`,
+        `${folder}/5.png`,
     ],
 
     treatment: [
-        'images/addiction.png',
-        'images/resources.png',
+        `${folder}/6.png`,
+        `${folder}/7.png`,
     ]
 
 };
