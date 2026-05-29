2ae1b167a589e9274cd0ae76de6837326c4c1ddd
Somos.Cv <somoscvapp@gmail.com>
2026-05-29 15:49:38 -0400

    Update lightshow-images.js

--- data/lightshow-images.js
+++ data/lightshow-images.js
@@ -1,25 +1,18 @@
-const isMobile = window.innerWidth <= 1024;
+function getLightshowFolder(){
+    const portrait = window.matchMedia("(orientation: portrait)").matches;
+    return portrait
+        ? "images/9-16-vertical"
+        : "images/9-16-horizontal";
+}
 
-const folder = isMobile
-    ? 'images/9-16-vertical'
-    : 'images/16-9-horizontal';
+function buildGallery(){
+    const folder = getLightshowFolder();
+    return {
+        recovery:[`${folder}/1.png`,`${folder}/2.png`,`${folder}/3.png`],
+        overdose:[`${folder}/4.png`,`${folder}/5.png`],
+        treatment:[`${folder}/6.png`,`${folder}/7.png`]
+    };
+}
 
-window.LIGHTSHOW_IMAGES = {
-
-    recovery: [
-        `${folder}/1.png`,
-        `${folder}/2.png`,
-        `${folder}/3.png`,
-    ],
-
-    overdose: [
-        `${folder}/4.png`,
-        `${folder}/5.png`,
-    ],
-
-    treatment: [
-        `${folder}/6.png`,
-        `${folder}/7.png`,
-    ]
-
-};
+window.getLightshowImages = buildGallery;
+window.LIGHTSHOW_IMAGES = buildGallery();
